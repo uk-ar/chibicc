ba03d183baaed61d8f080154976b697e3189d4cc
uk-ar <yuuki.ari@gmail.com>
2022-08-06 09:18:18 +0900

    #2 support operator + and -

--- 9cc.c
+++ 9cc.c
@@ -9,7 +9,22 @@ int main(int argc,char **argv){
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n",atoi(argv[1]));
+
+    char *p=argv[1];
+    printf("  mov rax, %d\n",strtol(p,&p,10));
+    while(*p){
+        if(*p=='-'){
+            p++;
+            printf("  sub rax, %d\n",strtol(p,&p,10));
+            continue;
+        }
+        if(*p=='+'){
+            p++;
+            printf("  add rax, %d\n",strtol(p,&p,10));
+            continue;
+        }
+        fprintf(stderr,"unexpected token\n.");
+    }
     printf("  ret\n");
     return 0;
 }
